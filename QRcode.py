48ae6a36e8365555353cf2b529441a6065230032
shubhamgyd <108620698+shubhamgyd@users.noreply.github.com>
2023-02-08 22:41:28 +0530

    Written Comment on the project what the terms mean

--- QRcode.py
+++ QRcode.py
@@ -1,3 +1,6 @@
+'''This Code is to generate QR (Quick Response) code  for any URL you 
+   paste in make() function and with the help of save that 
+   QR with name dot extension.Generally png is preferred for QR code.'''
 import qrcode as qr
 img = qr.make("https://www.youtube.com/playlist?list=PLjVLYmrlmjGfAUdLiF2bQ-0l8SwNZ1sBl")
 img.save("Python_project.png")
